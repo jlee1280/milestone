13e226451d5125c46f3c55931f546fccf5ab7e92
Jimmy <jlee1280@my.bcit.ca>
2021-11-28 19:01:57 -0800

    return promises

--- question2/IOhandler.js
+++ question2/IOhandler.js
@@ -21,11 +21,16 @@ const unzipper = require("unzipper"),
  * @return {promise}
  */
 const unzip = (pathIn, pathOut) => {
-  fs.createReadStream('path/to/archive.zip')
-  .pipe(unzipper.Extract({ path: 'output/path' }));
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(pathIn)
+     .pipe(unzipper.Extract({ path: pathOut}))
+     .on("close", () => {
+       resolve();
+     });
+  });
 };
 
-unzip("./myfile.zip", "unzipped");
+unzip("myfile.zip", "./unzipped");
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
@@ -34,21 +39,27 @@ unzip("./myfile.zip", "unzipped");
  * @return {promise}
  */
 
-var arr = [];
 
 const readDir = (dir) => {
+  return new Promise((resolve,reject) => {
   fs.readdir(dir, (err, files)=> {
-    if (err)
-    console.log(err);
-    else{
+    if (err) {
+    reject(err);
+    } else {
+      resolve(files);
+      let arr = [];
       files.forEach(file => {
-        if (path.extname(file) == ".png")
-        arr.push(file);
-        console.log(arr);
-      })
-    }
-  })
+        if (path.extname(file) === ".png") {
+        arr.push(path.join(dir, file));
+        return arr;
+        }
+      });
+     };
+    });
+  });
 };
+
+readDir("./unzipped")
 
 /**
  * Description: Read in png file by given pathIn,
@@ -58,8 +69,12 @@ const readDir = (dir) => {
  * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {
-  fs.createReadStream("in.png")
+const grayScale = (filePath, pathProcessed) => {
+  return new Promise((resolve,reject) => {
+  var fs = require("fs"),
+  PNG = require("pngjs").PNG;
+
+fs.createReadStream(filePath)
   .pipe(
     new PNG({
       filterType: 4,
@@ -69,20 +84,21 @@ const grayScale = (pathIn, pathOut) => {
     for (var y = 0; y < this.height; y++) {
       for (var x = 0; x < this.width; x++) {
         var idx = (this.width * y + x) << 2;
- 
+
         // invert color
         this.data[idx] = 255 - this.data[idx];
         this.data[idx + 1] = 255 - this.data[idx + 1];
         this.data[idx + 2] = 255 - this.data[idx + 2];
- 
         // and reduce opacity
         this.data[idx + 3] = this.data[idx + 3] >> 1;
       }
     }
- 
-    this.pack().pipe(fs.createWriteStream("out.png"));
+    this.pack().pipe(fs.createWriteStream(pathProcessed));
+    resolve();
+    });
   });
 };
+
 
 module.exports = {
   unzip,
